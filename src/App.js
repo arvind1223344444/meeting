e9c9ea49f8be94b6d18d1edc272b315ca86c5929
arvindrawatweb <arvindrawat5595@gmail.com>
2023-12-09 13:53:32 +0530

    fir

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import './App.css';
+import { ZoomMtg } from '@zoomus/websdk';
+
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.18.2/lib', '/av');
+
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
+// loads language files, also passes any error messages to the ui
+ZoomMtg.i18n.load('en-US');
+ZoomMtg.i18n.reload('en-US');
+
+function App() {
+
+  var authEndpoint = 'http://192.168.1.13:3000/tt/generate-zoom-signature'
+  var sdkKey = 'DuM6O6arTE6v6hg3aUJHhQ'
+  var meetingNumber = '86859981639'
+  var passWord = '0u3fKp'
+  var role = 1
+  var userName = 'Arvind'
+  var userEmail = 'arvindrawatdeveloper@gmail.com'
+  var registrantToken = ''
+  var zakToken = ''
+  var leaveUrl = 'http://localhost:4000'
+
+  function getSignature(e) {
+    e.preventDefault();
+
+    fetch(authEndpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        meetingNumber: meetingNumber,
+        role: role
+      })
+    }).then(res => res.json())
+    .then(response => {
+      startMeeting(response.signature)
+    }).catch(error => {
+      console.error(error)
+    })
+  }
+
+  function startMeeting(signature) {
+    document.getElementById('zmmtg-root').style.display = 'block'
+
+    ZoomMtg.init({
+      leaveUrl: leaveUrl,
+      success: (success) => {
+        console.log(success)
+
+        ZoomMtg.join({
+          signature: signature,
+          sdkKey: sdkKey,
+          meetingNumber: meetingNumber,
+          passWord: passWord,
+          userName: userName,
+          userEmail: userEmail,
+          tk: registrantToken,
+          zak: zakToken,
+          success: (success) => {
+            console.log(success)
+          },
+          error: (error) => {
+            console.error('golu', JSON.stringify(error, null, 2));
+
+          }
+        })
+
+      },
+      error: (error) => {
+        console.log('golu'+error)
+      }
+    })
+  }
+
+  return (
+    <div className="App">
+      <main>
+        <h1>Zoom Meeting SDK Sample React</h1>
+
+        <button onClick={getSignature}>Join Meeting</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
